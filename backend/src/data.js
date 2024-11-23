f07b04e28819dd97f4208dc13cc805ed11d82ea6
Richix <ricardomendez1919@gmail.com>
2024-11-23 09:40:28 -0600

    Inicia: Foodmine API

--- backend/src/data.js
+++ backend/src/data.js
@@ -1,4 +1,4 @@
-export const sample_foods = [
+/*export const sample_foods = [
   {
     id: '1',
     name: 'Pizza Pepperoni',
@@ -93,6 +93,107 @@ export const sample_users = [
     email: 'jane@gmail.com',
     password: '12345',
     address: 'Shanghai',
+    isAdmin: true,
+  },
+];
+*/
+import mongoose from 'mongoose';
+
+export const sample_foods = [
+  {
+    _id: new mongoose.Types.ObjectId(),  // Genera un ObjectId con mongoose
+    name: 'Pizza Pepperoni',
+    cookTime: '10-20',
+    price: 10,
+    favorite: false,
+    origins: ['italy'],
+    stars: 4.5,
+    imageUrl: 'food-1.jpg',
+    tags: ['FastFood', 'Pizza', 'Lunch'],
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),  // Genera un ObjectId con mongoose
+    name: 'Meatball',
+    price: 20,
+    cookTime: '20-30',
+    favorite: true,
+    origins: ['persia', 'middle east', 'china'],
+    stars: 5,
+    imageUrl: 'food-2.jpg',
+    tags: ['SlowFood', 'Lunch'],
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),  // Genera un ObjectId con mongoose
+    name: 'Hamburger',
+    price: 5,
+    cookTime: '10-15',
+    favorite: false,
+    origins: ['germany', 'us'],
+    stars: 3.5,
+    imageUrl: 'food-3.jpg',
+    tags: ['FastFood', 'Hamburger'],
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),  // Genera un ObjectId con mongoose
+    name: 'Fried Potatoes',
+    price: 2,
+    cookTime: '15-20',
+    favorite: true,
+    origins: ['belgium', 'france'],
+    stars: 3,
+    imageUrl: 'food-4.jpg',
+    tags: ['FastFood', 'Fry'],
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),  // Genera un ObjectId con mongoose
+    name: 'Chicken Soup',
+    price: 11,
+    cookTime: '40-50',
+    favorite: false,
+    origins: ['india', 'asia'],
+    stars: 3.5,
+    imageUrl: 'food-5.jpg',
+    tags: ['SlowFood', 'Soup'],
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),  // Genera un ObjectId con mongoose
+    name: 'Vegetables Pizza',
+    price: 9,
+    cookTime: '40-50',
+    favorite: false,
+    origins: ['italy'],
+    stars: 4.0,
+    imageUrl: 'food-6.jpg',
+    tags: ['FastFood', 'Pizza', 'Lunch'],
+  },
+];
+
+export const sample_tags = [
+  { name: 'All', count: 6 },
+  { name: 'FastFood', count: 4 },
+  { name: 'Pizza', count: 2 },
+  { name: 'Lunch', count: 3 },
+  { name: 'SlowFood', count: 2 },
+  { name: 'Hamburger', count: 1 },
+  { name: 'Fry', count: 1 },
+  { name: 'Soup', count: 1 },
+];
+
+export const sample_users = [
+  {
+    _id: new mongoose.Types.ObjectId(),  // Genera un ObjectId con mongoose
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    password: '12345',
+    address: 'Toronto On',
+    isAdmin: false,
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),  // Genera un ObjectId con mongoose
+    name: 'Ricardo Mendez',
+    email: 'Ricardo@gmail.com',
+    password: '12345',
+    address: 'Shanghai Merida',
     isAdmin: true,
   },
 ];
